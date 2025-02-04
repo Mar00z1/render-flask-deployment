608e33331c17f1deeb1b5872d3c09fb00d97a61f
Mar00z1 <marotrexsl165@gmail.com>
2025-02-04 19:09:59 -0300

    Add files via upload

---
+++ migrations/versions/e02cbdf857b0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e02cbdf857b0
+Revises: 0ef8198539be
+Create Date: 2025-02-04 19:09:17.723516
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e02cbdf857b0'
+down_revision = '0ef8198539be'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
