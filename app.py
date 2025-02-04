68969c52863380f8536a01ff1a4be595dac5f5f9
Mar00z1 <marotrexsl165@gmail.com>
2025-02-04 16:07:24 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ login_manager.login_view = 'login'  # Ruta a la que se redirige si no se está a
 
 # Modelo de Usuario (para autenticación)
 class User(db.Model, UserMixin):
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
@@ -61,7 +62,7 @@ class Message(db.Model):
     role = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 ##############################################
 # Configuración de Flask-Login               #
